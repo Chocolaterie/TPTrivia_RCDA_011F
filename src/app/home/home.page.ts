e8828d4ea0b54cd1bf1190bc1c1fa3d81c535220
Chocolaterie <110991127+Chocolaterie@users.noreply.github.com>
2023-10-02 15:59:59 +0200

    Correction TP 1

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,13 +1,39 @@
 import { Component } from '@angular/core';
 import { IonicModule } from '@ionic/angular';
+import { FormsModule } from '@angular/forms';
+import { NgForOf, NgSwitchCase, NgSwitch } from '@angular/common';
+
+export enum State { WAIT, PLAY }
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
   standalone: true,
-  imports: [IonicModule],
+  imports: [IonicModule, NgForOf, FormsModule, NgSwitchCase, NgSwitch],
 })
 export class HomePage {
-  constructor() {}
+
+  pseudo: String = "";
+  difficulties = ["Easy", "Medium", "Hard"]
+  errorText: String = "";
+  currentState = State.WAIT
+
+  // Ok wtf
+  State = State
+
+  constructor() { }
+
+  onSubmit() {
+    // Si le pseuo est pourri
+    if (this.pseudo.length < 4) {
+      // Erreur
+      this.errorText = "Veuillez rentrer un pseudo de 3 caractères minimum"
+
+    }
+    else {
+      // on joue
+      this.currentState = State.PLAY;
+    }
+  }
 }
